37d9a69e863d3379942eb02c47bcbf76df43b921
Reid Herrera <reidherrerabusiness@gmail.com>
2024-06-30 18:52:54 -0500

    implimented another feature, add breakfast. Also incorporated the checkin ability. This was a lot of work and added a ton of conditionals to avoid bugs and just an overall better user experience. I was able to use mutation functions to dynamically update the the most current version of the backend tables and was able to calculate the new value dynamically across all tables for a smooth process. :)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { StyleSheetManager } from "styled-components";
+import { Toaster } from "react-hot-toast";
 
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -12,10 +14,9 @@ import Settings from "./pages/Settings";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
-import { Toaster } from "react-hot-toast";
 import isPropValid from "@emotion/is-prop-valid";
-import { StyleSheetManager } from "styled-components";
 import Booking from "./pages/Booking";
+import Checkin from "./pages/Checkin";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -45,6 +46,7 @@ function App() {
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="bookings" element={<Bookings />} />
               <Route path="bookings/:bookingId" element={<Booking />} />
+              <Route path="checkin/:bookingId" element={<Checkin />} />
               <Route path="cabins" element={<Cabins />} />
               <Route path="users" element={<Users />} />
               <Route path="account" element={<Account />} />
