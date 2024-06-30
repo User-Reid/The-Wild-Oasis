be121519ceb386c0ef9d40301caf6b738686bf02
Reid Herrera <reidherrerabusiness@gmail.com>
2024-06-30 17:11:47 -0500

    added Booking page where staff can monitor and see guest full details by clicking on the icon at the end of the Cabin row and inspect it. Once it it then shows full booking details.

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import AppLayout from "./ui/AppLayout";
 import { Toaster } from "react-hot-toast";
 import isPropValid from "@emotion/is-prop-valid";
 import { StyleSheetManager } from "styled-components";
+import Booking from "./pages/Booking";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -43,6 +44,7 @@ function App() {
               <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="bookings" element={<Bookings />} />
+              <Route path="bookings/:bookingId" element={<Booking />} />
               <Route path="cabins" element={<Cabins />} />
               <Route path="users" element={<Users />} />
               <Route path="account" element={<Account />} />
