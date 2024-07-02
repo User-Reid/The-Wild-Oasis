2366bfb33fc2842e9fbf4188cbb189c3537a0f4e
Reid Herrera <reidherrerabusiness@gmail.com>
2024-07-01 20:08:11 -0500

    implimented true authentification and login functionality. Wrapped entire component in authenticated route to prevent users who do not possess the role of authenticated from the session object data property.

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import AppLayout from "./ui/AppLayout";
 import isPropValid from "@emotion/is-prop-valid";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -41,7 +42,13 @@ function App() {
         <GlobalStyles />
         <BrowserRouter>
           <Routes>
-            <Route element={<AppLayout />}>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               <Route index element={<Navigate replace to="dashboard" />} />
               <Route path="dashboard" element={<Dashboard />} />
               <Route path="bookings" element={<Bookings />} />
